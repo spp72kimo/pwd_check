3e732248f5dfe79412b7c93c3a6b988a0a754081
Steven Ke <spp72kimo@gmail.com>
2021-12-26 15:29:05 +0800

    增加錯誤輸入次數

--- pwd_check.py
+++ pwd_check.py
@@ -1,6 +1,6 @@
 # 這是一個設定使用者密碼並且檢查輸入密碼的程式
 
-pwd = input('請設定您的密碼：')	# 設定使用者密碼
+pwd = input('請設定您的密碼：')				# 設定使用者密碼
 
 while True:
 	pwd_input = input('請確認您的密碼：')		# 確認使用者設定的密碼
@@ -12,11 +12,17 @@ while True:
 
 
 pwd_check = input('請輸入您設定的密碼：')
+i = 3
 while True:
-	if pwd_check == pwd:	# 檢查密碼是否輸入正確
+	if pwd_check == pwd:					# 檢查密碼是否輸入正確
 		print('密碼正確！')
 		break	
-	else:
+	elif i > 0:								# 給3次機會重新輸入密碼
 		print('密碼錯誤！')
 		print('\n')
+		print('您還有',i,'次機會！')
+		i = i - 1
 		pwd_check = input('請重新輸入密碼：')
+	elif i == 0:							# 密碼錯誤3次，鎖定帳戶
+		print('輸入錯誤，已鎖定帳戶！')
+		break
